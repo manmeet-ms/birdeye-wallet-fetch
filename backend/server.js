2bc982b8003fa095bee489d7ddd02bb729269b62
Manmeet Singh <manmeets.zsh@gmail.com>
2025-03-13 00:07:09 +0530

    better UI

--- backend/server.js
+++ backend/server.js
@@ -12,56 +12,58 @@ const port = 3000;
 
 app.use(cors());
 
-
- 
 const API = new BlockFrostAPI({
     projectId: 'preprodyZkQeFwTnYqObSyTgkKaTnxBddHlnXr3', // see: https://blockfrost.io
 });
 
-const lace_addr = {
-    name: 'LACE',
-    wallet_address: 'addr_test1qqspc7gcc9mf4v63neuwjf8w0v9ddwts5gcqne5upfm77yjw7qfjqhfy533eyp4nsudyrz2ejvgd3ea9032he2tlhqmqa09rdw',
-};
-const eternl_addr = {
-    name: 'ETERNL',
-    wallet_address: 'addr_test1qpxagqfzacuyv2jw8jpalrkzfafy8mmcrma4v9fj3xz4c2876zqvaxgtq0n4wspdq4atxedf4vfpaxq30lz3k8rjqsusgr9an6',
-};
-const vespr_addr = {
-    name: 'VESPR',
-    wallet_address: 'addr_test1qrrlnua5zkeh9y2nx6aaxynu0mnme02hsdyt5a65x4qwj0z9g6up9src0eqa2wze99fayehwr0886dehe4yxux6m5g9qyg6tsc',
-};
-const tyhpon_addr = {
-    name: 'TYHPON',
-    wallet_address: 'addr_test1qq6jkv58lkkxhutxx4f2q6a46zq63cut60f0e2lf5jvkykllpxyhken3huvvgpdyylazljnd4g6k3t0cjtsq5gsul2hslzun5n',
-};
-const yoroi_addr = {
-    name: 'YOROI',
-    wallet_address: 'addr_test1qrt3vdtc8n3uvhrd34a40y2eg584slkyttehuntnum48s8257hn8sxtae66j0g859v2sgnkyyg6hk4k3fd0mrv6putesyxzl8e',
-};
-const gero_addr = {
-    name: 'GERO',
-    wallet_address: 'addr_test1qppl2kcdqmzwt3yzdcndfuhg9acx5zgfde76882magqyvxs428mpyg0ce8pn72s9gx7hhmzn3n0hetcr863qd2d4j44sdkf9zt',
-};
-
-const wallets = [lace_addr, eternl_addr, vespr_addr, gero_addr, tyhpon_addr];
+const wallets = [
+    {
+        wallet: 'LACE',
+        address: 'addr_test1qqspc7gcc9mf4v63neuwjf8w0v9ddwts5gcqne5upfm77yjw7qfjqhfy533eyp4nsudyrz2ejvgd3ea9032he2tlhqmqa09rdw',
+    },
+    {
+        wallet: 'ETERNL',
+        address: 'addr_test1qpxagqfzacuyv2jw8jpalrkzfafy8mmcrma4v9fj3xz4c2876zqvaxgtq0n4wspdq4atxedf4vfpaxq30lz3k8rjqsusgr9an6',
+    },
+    {
+        wallet: 'VESPR',
+        address: 'addr_test1qrrlnua5zkeh9y2nx6aaxynu0mnme02hsdyt5a65x4qwj0z9g6up9src0eqa2wze99fayehwr0886dehe4yxux6m5g9qyg6tsc',
+    },
+    {
+        wallet: 'TYHPON',
+        address: 'addr_test1qq6jkv58lkkxhutxx4f2q6a46zq63cut60f0e2lf5jvkykllpxyhken3huvvgpdyylazljnd4g6k3t0cjtsq5gsul2hslzun5n',
+    },
+   
+    {
+        wallet: 'GERO',
+        address: 'addr_test1qzfgfqw3adkg93ld6th5ndgq0fzp8fm3vgpzngrkxfytv6257hn8sxtae66j0g859v2sgnkyyg6hk4k3fd0mrv6putes0m7h97',
+    },
+];
 
 try {
-    const emptyarray_of_wallet_balances=[]
-    console.log('fetching wallets amounts');
+    const emptyarray_of_wallet_balances = [];
+    let curent_bal_as_string = new String();
+    let counter=1
+
     wallets.map(async (item) => {
-        const address = await API.addresses(item.wallet_address);
-        let current_balance = `${item.name} ${address.amount[0].quantity/1000000}` 
-        let current_balance_as_json = {
-            wallet_name:item.name,
-wallet_balance:address.amount[0].quantity/1000000
-            }
-         
-        emptyarray_of_wallet_balances.push(current_balance_as_json)
-        console.log(current_balance);
-        app.get('/', (req, res) => {
-            res.send(emptyarray_of_wallet_balances);
-        });
+        const address = await API.addresses(item.address);
+        let current_balance = `${counter}. ${item.wallet}: \u20B3${address.amount[0].quantity / 1000000}`;
+        curent_bal_as_string += `\n${current_balance}`;
+        console.log(curent_bal_as_string);
+        counter+=1
         
+        let current_balance_as_json = {
+            wallet: item.wallet,
+            balance: address.amount[0].quantity / 1000000,
+        };
+        
+        emptyarray_of_wallet_balances.push(current_balance_as_json);
+        // console.log(current_balance); //mapping logs
+        app.get('/', (req, res) => {
+            // res.send(emptyarray_of_wallet_balances);
+            res.send(curent_bal_as_string);
+        });
+
         return true;
     });
     // const address = await API.addresses('addr_test1qpxagqfzacuyv2jw8jpalrkzfafy8mmcrma4v9fj3xz4c2876zqvaxgtq0n4wspdq4atxedf4vfpaxq30lz3k8rjqsusgr9an6');
@@ -73,11 +75,10 @@ wallet_balance:address.amount[0].quantity/1000000
     console.log('success');
 }
 
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-    /*
+/*
     
     try {
         const latestBlock = await API.blocksLatest();
--- src/components/WalletsDocs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WalletsDocs = () => {
-  return (
-    <div>WalletsDocs</div>
-  )
-}
-
-export default WalletsDocs
